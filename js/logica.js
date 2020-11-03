ce60db8b25732d13e8da98ac7d44643354eb9bb2
Axios01 <61210965+Axios01@users.noreply.github.com>
2020-11-02 22:23:15 -0600

    Update logica.js
    Versión chida

--- js/logica.js
+++ js/logica.js
@@ -1,49 +1,63 @@
-//vamos a utilizar javascript ver 6, apartir de funciones
-//de tipo callback y funciones anonimas
 
 var cesar = cesar || (function(){
-    //una funcion que no tiene nombre, porque le da penita
-    
-    //funcion para la operacion del cifrado
-    //texto, desp, accion
+  
     var doStaff = function(txt, desp, action){
-        //otra variable que se encargue del reemplazo de
-        //la cadena origianl para realizar los movimientos
-        //del cifrado
+        
+        var regul = /[0-9\d]/;
+        
+        if (!regul.test(desp)) {
+            alert("Error de formato en el número de salto");
+            return "";
+        }
+        var shift = parseInt(desp, 10);
+        if (shift>26) {
+            alert("El número de salto no puede ser mayor a 26");
+            return "";
+        }
+
         var replace = (function(){
-            //definir nuestro alfabeto
+           
             var abc = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j',
         'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',  'v', 'w', 'x',
         'y', 'z'];
-            //saber la longitud
-            var l = abc.length;
-            //voya  retornar de mi funcion
+            
+            var le = abc.length;
+            var shift = parseInt(desp, 10);
+            
+
             return function(c){
-                //aqui adentro vamos a realizar la logica
-                //que se encarga del cifrado y descifrado
+                
                 var i = abc.indexOf(c.toLowerCase());
-                //saber si esta vacio el campo
+                //console.log("Index: "+i);
+            
+                //console.log("AHHHHH PTM: " + abc.indexOf(c.toLowerCase()));
+                /*for (let i2 = 0; i2 < le; i2++) {
+                    console.log("Qué está pasando: " + abc[(pos + shift) % le]);
+                  }
+                */
+               
                 if(i != -1){
-                    //no esta vacia
+                    
                     var pos = i;
-                    //cifrar o descifrar
+                    
                     if(action){
-                        //cifrando
-                        pos += desp;
-                        pos -= (pos>=1)?1:0;
+                        
+                        pos = (pos + shift) % le;                      
+                        //console.log("AVR: " + pos);
                     }else{
-                        //descifrando
-                        pos -= desp;
-                        pos += (pos<0)?1:0;
+                        
+                        pos = (pos - shift) % le;
+                        if(pos<0){
+                            i = le - shift + i;
+                            pos = i;
+                        }
                     }
                     return abc[pos];
                 }
                 return c;
             };
         })();
-        //tenemos que realizar una prueba del texto que estan
-        //escribiendo en el textarea para que sea
-        //solo lo que yo quiero
+        
         var re = (/([a-z])/ig);
         return String(txt).replace(re, function(match){
             return replace(match);
@@ -51,10 +65,6 @@ var cesar = cesar || (function(){
 
         
     }
-    //necesiatar saber que estoy mandando
-        //cifrado o descifrado, eso viene apartir de
-        //el boton que tiene una funcion code y otra deco
-
         return{
             encode : function(txt, desp){
                 return doStaff(txt, desp, true);
@@ -65,16 +75,15 @@ var cesar = cesar || (function(){
         };
 })();
 
-//voy a crear mis funciones de cifrado
 
 function codificar(){
     document.getElementById('resultado').innerHTML = cesar.encode(
-        document.getElementById('cadena').value, 3
+        document.getElementById('cadena').value, document.getElementById('shift').value
     );
 }
 
 function decodificar(){
     document.getElementById('resultado').innerHTML = cesar.decode(
-        document.getElementById('cadena').value, 3
+        document.getElementById('cadena').value, document.getElementById('shift').value
     );
 }
